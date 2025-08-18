d3fcd255213e92ba737cda944d0c584f74d8b400
THMoy <1491899066@qq.com>
2025-08-18 21:46:57 +0800

    Site updated: 2025-08-18 21:46:57

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1755524745491";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1755524812833";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
